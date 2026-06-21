f6d964674cca5535c975728552ce6ab52fed5a53
wahid <wahidqandill@gmail.com>
2026-06-21 14:52:35 +0300

    Reflect new local data producer classes into app graph to be injected/retreived

--- shared/src/commonMain/kotlin/com/wahid/newscmp/di/AppGraph.kt
+++ shared/src/commonMain/kotlin/com/wahid/newscmp/di/AppGraph.kt
@@ -1,6 +1,9 @@
 package com.wahid.newscmp.di
 
-import com.wahid.newscmp.data.remote.NewsRemoteDatasource
+import com.wahid.newscmp.data.local.datasource.NewsLocalDatasource
+import com.wahid.newscmp.data.local.room.NewsDatabase
+import com.wahid.newscmp.data.local.room.dao.NewsDao
+import com.wahid.newscmp.data.remote.datasource.NewsRemoteDatasource
 import com.wahid.newscmp.domain.repository.NewsRepository
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.DependencyGraph
@@ -9,6 +12,9 @@ import io.ktor.client.HttpClient
 @DependencyGraph(AppScope::class)
 interface AppGraph{
     val ktorClient: HttpClient
+    val newsDatabase: NewsDatabase
+    val newsDao: NewsDao
     val newsRemoteDatasource: NewsRemoteDatasource
+    val newsLocalDatasource: NewsLocalDatasource
     val newsRepository: NewsRepository
 }
