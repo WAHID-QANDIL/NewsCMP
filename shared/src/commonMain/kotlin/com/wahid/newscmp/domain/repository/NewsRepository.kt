057088f4b6a79b277e9998f645cb6b3320df9282
wahid <wahidqandill@gmail.com>
2026-06-21 14:51:34 +0300

    Finish NewsRepository Implementation, and added local cache strategy

--- shared/src/commonMain/kotlin/com/wahid/newscmp/domain/repository/NewsRepository.kt
+++ shared/src/commonMain/kotlin/com/wahid/newscmp/domain/repository/NewsRepository.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
 
-    fun getAllNews(queryFilter: Map<String, String>): Flow<List<Article>>
+    fun getAllNews(queryFilter: Map<String, String>,forceFetch: Boolean): Flow<List<Article>>
     fun getHeadLinesNews(queryFilter: Map<String, String>): Flow<List<Article>>
 
 }
