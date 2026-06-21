7633db7c3bf1419c01174e543d589db44505fb62
wahid <wahidqandill@gmail.com>
2026-06-21 14:48:00 +0300

    Add ArticleExt.kt

--- shared/src/commonMain/kotlin/com/wahid/newscmp/mappers/ArticleExt.kt
+++ shared/src/commonMain/kotlin/com/wahid/newscmp/mappers/ArticleExt.kt
@@ -1,5 +1,6 @@
 package com.wahid.newscmp.mappers
 
+import com.wahid.newscmp.data.local.room.entity.ArticleEntity
 import com.wahid.newscmp.domain.model.Article
 import kotlin.time.Clock
 
@@ -23,3 +24,27 @@ fun com.wahid.newscmp.data.remote.dto.Article.toDomainModel(): Article {
         urlToImage = urlToImage?: ""
     )
 }
+fun com.wahid.newscmp.data.remote.dto.Article.toDatabaseEntity( isFavorite: Boolean = false): ArticleEntity = ArticleEntity(
+    id = getCacheKey(),
+    isFavorite = isFavorite,
+    author = author ?: "No author info found",
+    content = content ?: "No content",
+    description = description ?: "No description",
+    publishedAt = publishedAt ?: "No publishedAt info found",
+    title = title ?: "No title",
+    url = url ?: "",
+    urlToImage = urlToImage?: "",
+    lastUpdate = Clock.System.now()
+)
+
+
+fun ArticleEntity.toDomainModel(): Article = Article(
+    id = id,
+    author = author,
+    content = content,
+    description = description,
+    publishedAt = publishedAt,
+    title = title,
+    url = url,
+    urlToImage = urlToImage
+)
